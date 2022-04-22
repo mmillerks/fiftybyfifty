1c25cdaea871498073b919f12726ad4df4194982
Mindy Miller <mmillerks@gmail.com>
2022-04-21 20:51:57 -0500

    state list updated thru new jersey

--- src/components/StateList/StateList.js
+++ src/components/StateList/StateList.js
@@ -5,23 +5,22 @@ export default function StateList(props) {
       <div>
 
           <div class="states">
-          Select the states you have visited. <br/>
+          Select the states you have visited. <br/> <br/>
 
              <input type="checkbox" name="Alabama" value="yes"/>
-             <label for="Alabama">Alabama</label>
+             <label for="Alabama">Alabama</label> <br/>
               
              <input type="checkbox" name="Alaska" value="yes"/>
-             <label for="Alaska">Alaska</label>
+             <label for="Alaska">Alaska</label> <br/>
 
-              
              <input type="checkbox" name="Arizona" value="yes"/>
-             <label for="Arizona">Arizona</label>
+             <label for="Arizona">Arizona</label> <br/>
 
              <input type="checkbox" name="Arkansas" value="yes"/>
-             <label for="Arkansas">Arkansas</label>
+             <label for="Arkansas">Arkansas</label> <br/>
 
              <input type="checkbox" name="California" value="yes"/>
-             <label for="California">California</label>
+             <label for="California">California</label> <br/>
 
              <input type="checkbox" name="Colorado" value="yes"/>
              <label for="Colorado">Colorado</label>
@@ -31,6 +30,74 @@ export default function StateList(props) {
 
              <input type="checkbox" name="Delaware" value="yes"/>
              <label for="Delaware">Delaware</label>
+
+             <input type="checkbox" name="Florida" value="yes"/>
+             <label for="Florida">Florida</label>
+
+             <input type="checkbox" name="Georgia" value="yes"/>
+             <label for="Georgia">Georgia</label>
+
+             <input type="checkbox" name="Hawaii" value="yes"/>
+             <label for="Hawaii">Hawaii</label>
+
+             <input type="checkbox" name="Idaho" value="yes"/>
+             <label for="Idaho">Idaho</label>
+
+             <input type="checkbox" name="Illinois" value="yes"/>
+             <label for="Illinois">Illinois</label>
+
+             <input type="checkbox" name="Indiana" value="yes"/>
+             <label for="Indiana">Indiana</label>
+
+             <input type="checkbox" name="Iowa" value="yes"/>
+             <label for="Iowa">Iowa</label>
+
+             <input type="checkbox" name="Kansas" value="yes"/>
+             <label for="Kansas">Kansas</label>
+
+             <input type="checkbox" name="Kentucky" value="yes"/>
+             <label for="Kentucky">Kentucky</label>
+
+             <input type="checkbox" name="Louisiana" value="yes"/>
+             <label for="Louisiana">Louisiana</label>
+
+             <input type="checkbox" name="Maine" value="yes"/>
+             <label for="Maine">Maine</label>
+
+             <input type="checkbox" name="Maryland" value="yes"/>
+             <label for="Maryland">Maryland</label>
+
+             <input type="checkbox" name="Massachusetts" value="yes"/>
+             <label for="Massachusetts">Massachusetts</label>
+
+             <input type="checkbox" name="Michigan" value="yes"/>
+             <label for="Michigan">Michigan</label>
+
+             <input type="checkbox" name="Minnesota" value="yes"/>
+             <label for="Minnesota">Minnesota</label>
+
+             <input type="checkbox" name="Mississippi" value="yes"/>
+             <label for="Mississippi">Mississippi</label>
+
+             <input type="checkbox" name="Missouri" value="yes"/>
+             <label for="Missouri">Missouri</label>
+
+             <input type="checkbox" name="Montana" value="yes"/>
+             <label for="Montana">Montana</label>
+
+             <input type="checkbox" name="Nebraska" value="yes"/>
+             <label for="Nebraska">Nebraska</label>
+
+             <input type="checkbox" name="Nevada" value="yes"/>
+             <label for="Nevada">Nevada</label>
+
+             <input type="checkbox" name="New Hampshire" value="yes"/>
+             <label for="New Hampshire">New Hampshire</label>
+
+             <input type="checkbox" name="New Jersey" value="yes"/>
+             <label for="New Jersey">New Jersey</label>
+
+
             
           </div>
 
