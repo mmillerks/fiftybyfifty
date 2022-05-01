4ba336f465313dd49a1ba1c3d64848d9134cb8c3
Mindy Miller <mmillerks@gmail.com>
2022-05-01 09:49:06 -0500

    updates to featured pg

--- src/components/FeaturedLocation/FeaturedLocation.js
+++ src/components/FeaturedLocation/FeaturedLocation.js
@@ -1,30 +1,49 @@
 
 
-
-
 export default function FeaturedLocation(props) {
   return (
   <div id="featured">
-     <div>
-       <h1>Featured Location</h1>
-    </div> 
+     <div id="featuredMini">
+      <img src="https://i.imgur.com/zMnERYZ.png?1" />
+     </div> 
 
     <div id="featuredTitle">
-        <img src="https://i.imgur.com/jLvjXcL.png" />
+        <img src="https://i.imgur.com/jLvjXcL.png?1" />
     </div>
 
-    <div className="category">
-        <img src="https://i.imgur.com/wabObNR.png" />
+    <div id="tagline">
+      discover a playground for adventure where nature, culture, and living intersect
     </div>
 
-    <div className="category">
-      <img src="https://i.imgur.com/UKkDmKu.png" />
+
+    <div id="categorySect">
+        <div className="category">
+        <a href="https://www.nationalparks.org/connect/explore-parks/rocky-mountain-national-park?gclid=Cj0KCQjw37iTBhCWARIsACBt1Ix0GKBfVZ_neDR7BfKhhWE9-zNbJKcNFYnlAbuVcxJlEQ_HoyCa4bgaAuq6EALw_wcB">
+        <img src="https://i.imgur.com/wabObNR.png" /> </a>
+        </div>
+
+        <div className="category">
+          <a href="https://www.denver.org/">
+          <img src="https://i.imgur.com/UKkDmKu.png" />
+          </a>
+        </div>
+
+        <div className="category">
+          <a href="https://www.colorado.com/articles/11-must-visit-colorado-food-towns">
+          <img src="https://i.imgur.com/h3tzcl6.png" />
+          </a>
+        </div>
     </div>
 
-    <div className="category">
-       <img src="https://i.imgur.com/h3tzcl6.png" />
-    </div>
+    <div id="stateFavs">
+      <div>
+        <img src="https://i.imgur.com/olhY9rm.png" width={"250px"}/>
+      </div>
 
+      <div>
+
+      </div>
+    </div>
     
       
      
